8d0d9cd1d03aadcb8aa144489be064d5563834e5
zonaaps <129642977+zonaaps@users.noreply.github.com>
2025-08-16 14:49:21 -0500

    Update and rename Banner 300x250.html to Banner 300x250.js

---
+++ Banner 300x250.js
@@ -0,0 +1,14 @@
+(function() {
+  var atOptions = {
+    key: 'ca86deaf6593c88f0be696029bc4dbd7',
+    format: 'iframe',
+    height: 250,
+    width: 300,
+    params: {}
+  };
+
+  var s = document.createElement('script');
+  s.src = 'https://yawnfreakishnotably.com/ca86deaf6593c88f0be696029bc4dbd7/invoke.js';
+  s.async = true;
+  document.body.appendChild(s);
+})();
