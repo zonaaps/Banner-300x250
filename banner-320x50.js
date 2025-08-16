66437e92981d0a5a25e01e7eb3abc1ec995f666a
zonaaps <129642977+zonaaps@users.noreply.github.com>
2025-08-16 16:35:04 -0500

    Add files via upload

---
+++ banner-320x50.js
@@ -0,0 +1,11 @@
+<!-- banner-320x50.js -->
+<script type='text/javascript'>
+    atOptions = {
+        'key' : '3e39783dcd4d49e08deec38ca1079eab',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+    };
+</script>
+<script type='text/javascript' src='//yawnfreakishnotably.com/3e39783dcd4d49e08deec38ca1079eab/invoke.js'></script>
