93e8223574eede94d18e8c6aa7ca42e557d94cdb
zonaaps <129642977+zonaaps@users.noreply.github.com>
2025-08-16 15:06:19 -0500

    Update banner-300x250.js

--- banner-300x250.js
+++ banner-300x250.js
@@ -1,12 +1,11 @@
-// anuncio.js
-var atOptions = {
-    'key': 'ca86deaf6593c88f0be696029bc4dbd7',
-    'format': 'iframe',
-    'height': 250,
-    'width': 300,
-    'params': {}
-};
-var script = document.createElement('script');
-script.type = 'text/javascript';
-script.src = '//yawnfreakishnotably.com/ca86deaf6593c88f0be696029bc4dbd7/invoke.js';
-document.body.appendChild(script);
+<!-- anuncio.html -->
+<script type='text/javascript'>
+    atOptions = {
+        key: 'ca86deaf6593c88f0be696029bc4dbd7',
+        format: 'iframe',
+        height: 250,
+        width: 300,
+        params: {}
+    };
+</script>
+<script type='text/javascript' src='//yawnfreakishnotably.com/ca86deaf6593c88f0be696029bc4dbd7/invoke.js'></script>
